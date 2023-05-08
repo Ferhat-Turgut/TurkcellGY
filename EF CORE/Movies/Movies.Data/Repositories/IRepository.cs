85f2208fbde9e35e7ddebd15124fc0bdaf38380d
Ferhat-Turgut <turgutferhat.ft@gmail.com>
2023-05-08 16:38:54 +0300

    Add project files.

---
+++ EF CORE/Movies/Movies.Data/Repositories/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using Movies.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Data.Repositories
+{
+    public interface IRepository<T> where T: class,IEntity,new()
+    {
+        
+        Task CreateAsync(T entity);
+        //void Create(T entity);  Hem senkron hem asenkron olarak aynı anda tanımlanabilir.
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+        Task<IList<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+    }
+}
